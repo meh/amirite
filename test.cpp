a042a5af7b63a5bb24c8661b28dac3d6ad4c600d
meh <meh@schizofreni.co>
2013-03-13 22:21:13 +0100

    test: simplify code

--- test.cpp
+++ test.cpp
@@ -4,17 +4,7 @@
 int
 main (int argc, char* argv[])
 {
-	amirite("duh", {
-		{ "dah", []{
-			ami::truthy(23);
-		}},
-
-		{ "deh", []{
-			ami::truthy(0);
-		}}
-	});
-
-	amirite(ami::printer::prelude, ami::printer::dots, {
+	return amirite("duh", {
 		{ "dah", []{
 			ami::truthy(23);
 		}},
